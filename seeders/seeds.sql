a13f302448a8911bcd98d73d200f925d362ceb94
Joe HurowitzE <joehurowitz@gmail.com>
2017-05-24 00:03:16 -0400

    HW update

---
+++ seeders/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Cheesburger');
+INSERT INTO burgers (burger_name) VALUES ('Veggieburger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Tempeh',true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Southwest',true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bluecheese',true);
+INSERT INTO burgers (burger_name) VALUES ('Blackbean');
